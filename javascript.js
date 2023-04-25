88b7047e708bf0e1000bab47db0c7574b7385154
Patrolox <122224067+Patrolox@users.noreply.github.com>
2023-04-25 21:44:38 +0200

    Added code

--- javascript.js
+++ javascript.js
@@ -1,30 +1,72 @@
-function getComputerChoice() {
-  // random value for computer player
+const buttons = document.querySelectorAll("button");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const resultDisplay = document.getElementById("result");
+const winnerDisplay = document.getElementById("winner");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+  // Randomly return rock, paper, or scissors
   const choices = ["Rock", "Paper", "Scissors"];
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// ask for user input
+function playRound(playerSelection) {
+  console.log(`You played: ${playerSelection}`);
+  const computerSelection = computerPlay();
+  console.log(`PC played: ${computerSelection}`);
+  let result;
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection.toLowerCase()) {
-    console.log("TIE!");
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+    result = "It's a tie!";
   } else if (
-    (playerSelection === "rock" && computerSelection === "Scissors") ||
-    (playerSelection === "paper" && computerSelection === "Rock") ||
-    (playerSelection === "scissors" && computerSelection === "Paper")
-  )
-    return "You win!";
-  else return "You loose";
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    playerScore++;
+    playerScoreDisplay.textContent = playerScore;
+    result = `You win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+  } else {
+    computerScore++;
+    computerScoreDisplay.textContent = computerScore;
+    result = `You lose! ${computerSelection} beats ${playerSelection}`;
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  }
+  resultDisplay.textContent = result;
+  if (playerScore === 5) {
+    winnerDisplay.textContent = "Congratulations, you're the winner!";
+    disableButtons();
+  } else if (computerScore === 5) {
+    winnerDisplay.textContent = "Sorry, the computer wins!";
+    disableButtons();
+  }
+}
+function disableButtons() {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
+function logButton(buttonId) {
+  console.log(`Button ${buttonId} pressed!`);
 }
 
-for (let i = 0; i < 5; i++) {
-  const playerSelection = prompt("Please choose: rock, paper or scissors");
-  const computerSelection = getComputerChoice();
-  console.log(`Round ${i + 1}`);
-  console.log(`You chose: ${playerSelection}`);
-  console.log(`PC chose: ${computerSelection}`);
-  console.log(playRound(playerSelection, computerSelection));
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    logButton(button.textContent);
+    playRound(button.textContent);
+  });
+});
+
+// for (let i = 0; i < 5; i++) {
+//   const playerSelection = prompt("Please choose: rock, paper or scissors");
+//   const computerSelection = getComputerChoice();
+//   console.log(`Round ${i + 1}`);
+//   console.log(`You chose: ${playerSelection}`);
+//   console.log(`PC chose: ${computerSelection}`);
+//   console.log(playRound(playerSelection, computerSelection));
+// }
